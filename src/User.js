e2bb09c691885e73f9ae8a7590036ca2fd7bfb30
Joan Rasmussen <raz.joan@gmail.com>
2021-10-16 15:48:06 -0600

    Add constructor method to User class

---
+++ src/User.js
@@ -0,0 +1,13 @@
+class User {
+  constructor(user) {
+    this.id = user.id;
+    this.name = user.name;
+    this.address = user.address;
+    this.email = user.email;
+    this.strideLength = user.strideLength;
+    this.dailyStepGoal = user.dailyStepGoal;
+    this.friends = user.friends;
+  }
+}
+
+module.exports = User;
--- test/User-test.js
+++ test/User-test.js
@@ -60,60 +60,60 @@ describe('User', function() {
     };
   });
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     expect(User).to.be.a('function');
   });
 
-  it.skip('should be an instance of User', function() {
+  it('should be an instance of User', function() {
     let userA = new User(user1);
     expect(userA).to.be.an.instanceof(User);
     let userB = new User(user4);
     expect(userB).to.be.an.instanceof(User);
   });
 
-  it.skip('should store the user\'s id', function() {
+  it('should store the user\'s id', function() {
     let userA = new User(user1);
     expect(userA.id).to.equal(1);
     let userB = new User(user3);
     expect(userB.id).to.equal(3);
   });
 
-  it.skip('should store the user\'s name', function() {
+  it('should store the user\'s name', function() {
     let userA = new User(user4);
     expect(userA.name).to.equal("Mae Connelly");
     let userB = new User(user5);
     expect(userB.name).to.equal("Erick Schaden");
   });
 
-  it.skip('should store the user\'s address', function() {
+  it('should store the user\'s address', function() {
     let userA = new User(user2);
     expect(userA.address).to.equal("30086 Kathryn Port, Ciceroland NE 07273");
     let userB = new User(user3);
     expect(userB.address).to.equal("85823 Bosco Fork, East Oscarstad MI 85126-5660");
   });
 
-  it.skip('should store the user\'s email', function() {
+  it('should store the user\'s email', function() {
     let userA = new User(user2);
     expect(userA.email).to.equal("Dimitri.Bechtelar11@gmail.com");
     let userB = new User(user4);
     expect(userB.email).to.equal("Marcos_Pollich@hotmail.com");
   });
 
-  it.skip('should store the user\'s stride length', function() {
+  it('should store the user\'s stride length', function() {
     let userA = new User(user1);
     expect(userA.strideLength).to.equal(4.3);
     let userB = new User(user5);
     expect(userB.strideLength).to.equal(3.1);
   });
 
-  it.skip('should store the user\'s daily step goal', function() {
+  it('should store the user\'s daily step goal', function() {
     let userA = new User(user2);
     expect(userA.dailyStepGoal).to.equal(5000);
     let userB = new User(user5);
     expect(userB.dailyStepGoal).to.equal(8000);
   });
 
-  it.skip('should store the user\'s friends', function() {
+  it('should store the user\'s friends', function() {
     let userA = new User(user1);
     expect(userA.friends).to.deep.equal([2, 3, 4, 5]);
     let userB = new User(user3);

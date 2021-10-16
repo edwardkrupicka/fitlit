f2819109521c8f72683cc83a1c8b8a2fa60a644e
John Kiernan <jkiernan12@yahoo.com>
2021-10-16 14:13:14 -0400

    Add functionality to get a user and average step goal

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,5 +1,19 @@
 class UserRepository {
+  constructor(data) {
+    this.allUserData = data
+  }
 
+  getUser(findId) {
+    return this.allUserData.find(user => user.id === findId);
+  }
+
+  averageStepGoal() {
+    let totalSteps = this.allUserData.reduce((total, user) => {
+      return total += user.dailyStepGoal;
+    }, 0);
+    let totalUsers = this.allUserData.length;
+    return Math.round(totalSteps / totalUsers);
+  }
 }
 
 export default UserRepository;

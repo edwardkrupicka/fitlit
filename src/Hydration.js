a8a18a22f24c6b388363b6ab75b48b5b7100f91a
edward krupicka <edwardkrupicka@gmail.com>
2021-10-19 17:21:08 -0600

    Add properties and method to calculate avg oounces drank for all time

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,7 +1,18 @@
 class Hydration {
-  constructor() {
-
+  constructor(id, hydrationData) {
+    this.id = id;
+    this.hydrationData = hydrationData.filter(data => data.userID === this.id);
+  }
+  findAverageNumOunces() {
+    const numOuncesArray = this.hydrationData.map(data => data.numOunces);
+    const totalOunces = numOuncesArray.reduce((acc, ounces) => {
+      return acc += ounces;
+    }, 0);
+    console.log(totalOunces / numOuncesArray.length);
+    return totalOunces / numOuncesArray.length;
+    
   }
 };
 
-module.exports = Hydration;
+
+export default Hydration;

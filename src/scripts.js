eee03c7bc9070e75520a9797abf99dcc85077ced
John Kiernan <jkiernan12@yahoo.com>
2021-10-19 08:33:28 -0400

    Replace data source for users with fetch; update friends list

--- src/scripts.js
+++ src/scripts.js
@@ -11,13 +11,13 @@ console.log('This is the JavaScript entry file - your code begins here.');
 
 // An example of how you tell webpack to use a JS file
 
-import userData from './data/users';
+fetch('https://pacific-badlands-43237.herokuapp.com/api/v1/users')
+.then(req => req.json())
+.then(data => renderUser(data.userData));
 import UserRepository from './UserRepository';
 import User from './User';
 
 // Global
-const userRepo = new UserRepository(userData);
-const user = new User(userRepo.getUser(17));
 
 const userGreeting = document.querySelector('#userGreeting');
 const userFullName = document.querySelector('#userFullName');
@@ -28,17 +28,21 @@ const userFriends = document.querySelector('#userFriends');
 const userStepGoal = document.querySelector('#userStepGoal');
 const averageStepGoal = document.querySelector('#averageStepGoal');
 
-userGreeting.innerText = `Greetings ${user.returnFirstName()}`;
-userFullName.innerText = user.name;
-userEmail.innerText = user.email;
-userAddress.innerText = user.address;
-userStride.innerText = user.strideLength;
-userStepGoal.innerText = user.dailyStepGoal;
-averageStepGoal.innerText = userRepo.averageStepGoal();
-userFriends.innerHTML = addFriends();
+function renderUser(userData) {
+  const userRepo = new UserRepository(userData);
+  const user = new User(userRepo.getUser(17));
+  userGreeting.innerText = `Greetings ${user.returnFirstName()}`;
+  userFullName.innerText = user.name;
+  userEmail.innerText = user.email;
+  userAddress.innerText = user.address;
+  userStride.innerText = user.strideLength;
+  userStepGoal.innerText = user.dailyStepGoal;
+  averageStepGoal.innerText = userRepo.averageStepGoal();
+  userFriends.innerHTML = addFriends(user, userRepo);
+}
 
 
-function addFriends() {
+function addFriends(user, userRepo) {
   let friendsList = user.friends;
   return friendsList.reduce((finalString, friend) => {
     return finalString += `<li>${userRepo.getUser(friend).name}: Step Goal ${userRepo.getUser(friend).dailyStepGoal}</li>`

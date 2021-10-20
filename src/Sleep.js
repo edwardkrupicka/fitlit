773c486dccf418a533c6370cf0501aabf39247ee
jose Tovar <jose.tovar102397@gmail.com>
2021-10-19 20:12:09 -0600

    Add method to get the average sleep quality

--- src/Sleep.js
+++ src/Sleep.js
@@ -6,11 +6,21 @@ class Sleep {
   getAverageHoursSlept(userId) {
     const userSleep = this.allSleepData.filter(user => {
       return user.userID === userId;
-    })
+    });
     const averageHoursSlept = userSleep.reduce((avgHours, user) => {
       return avgHours += user.hoursSlept;
     }, 0) / userSleep.length;
     return averageHoursSlept;
+  }
+
+  getAverageSleepQuality(userId) {
+    const userSleepQuality = this.allSleepData.filter(user => {
+      return user.userID === userId;
+    });
+    const avgUserSleepQuality = userSleepQuality.reduce((avgSleep, user) => {
+      return avgSleep += user.sleepQuality;
+    }, 0) / userSleepQuality.length;
+    return avgUserSleepQuality;
   }
 }
 

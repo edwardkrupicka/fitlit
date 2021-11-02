1baca6f71ed150f8cbc32a9f459bf516d6d784dd
John Kiernan <jkiernan12@yahoo.com>
2021-11-02 08:56:40 -0400

    Add rest of tests for Activity

--- test/Activity-test-data.js
+++ test/Activity-test-data.js
@@ -9,4 +9,34 @@ const activityData = [{"userID":1,"date":"2019/06/15","numSteps":2634,"minutesAc
 {"userID":3,"date":"2019/06/21","numSteps":1300,"minutesActive":256,"flightsOfStairs":13},
 {"userID":3,"date":"2019/06/22","numSteps":26,"minutesActive":84,"flightsOfStairs":5}]
 
-export { activityData }
+const userData = [
+  {
+    "id": 1,
+    "name": "Luisa Hane",
+    "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+    "email": "Diana.Hayes1@hotmail.com",
+    "strideLength": 4.3,
+    "dailyStepGoal": 10000,
+    "friends": [2]
+  },
+  {
+    "id": 2,
+    "name": "Jarvis Considine",
+    "address": "30086 Kathryn Port, Ciceroland NE 07273",
+    "email": "Dimitri.Bechtelar11@gmail.com",
+    "strideLength": 4.5,
+    "dailyStepGoal": 5000,
+    "friends": [1]
+  },
+  {
+    "id": 3,
+    "name": "Bill Billson",
+    "address": "30086 Kathryn Port, Randomtown, NY",
+    "email": "bill@example.com",
+    "strideLength": 4.1,
+    "dailyStepGoal": 5000,
+    "friends": [1, 2]
+  },
+]
+
+export { activityData, userData }
--- test/Activity-test.js
+++ test/Activity-test.js
@@ -1,16 +1,20 @@
 import { expect } from 'chai';
 import Activity from '../src/Activity';
-import { activityData } from '../test/Activity-test-data';
+import User from '../src/User';
+import UserRepository from '../src/UserRepository';
+import { userData, activityData } from '../test/Activity-test-data';
 
 describe("Activity", () => {
-  let activity1;
-  let activity2;
-  let activity3;
+  let activity1, activity2, activity3, user1, user2, user3, userRepo;
 
   beforeEach(() => {
     activity1 = new Activity(1, activityData);
     activity2 = new Activity(2, activityData);
     activity3 = new Activity(3, activityData);
+    user1 = new User(userData[0]);
+    user2 = new User(userData[1]);
+    user3 = new User(userData[2]);
+    userRepo = new UserRepository(userData)
   });
 
   it('should be a function', () => {
@@ -30,4 +34,35 @@ describe("Activity", () => {
     expect(activity1.activityData).to.deep.equal(activityData.filter(data => data.userID === 1));
     expect(activity3.hydrationData).to.deep.equal(hydrationData.filter(data => data.userID === 3));
   });
+
+  it('should return total miles walked for a given day', () => {
+    expect(activity3.getMiles('2019/06/18').to.equal(1.72));
+  });
+  
+  it('should return the number of active minutes for a day', () => {
+    expect(activity3.getDayMins('2019/06/21').to.equal(256))
+  });
+
+  it('should return average mins per week per day', () => {
+    expect(activity3.getWeekAverage("2019/06/15").to.equal(1354))
+  });
+
+  it('should check if they reached their step goal', () => {
+    expect(activity2).checkSteps("2019/06/15").to.equal(false);
+    expect(activity3).checkSteps("2019/06/21").to.equal(true);
+  });
+
+  it('should find all the days they exceeded their step goal', () => {
+    expect(activity3.findWinStepDays().to.deep.equal(["2019/06/20", "2019/06/21"]));
+  });
+
+  it('should find their all-time stair climbing record', () => {
+    expect(activity3.findBestSteps().to.deep.equal(13));
+  });
+
+  it('should find the average steps, stairs and active minutes for a date range', () => {
+    expect(activity3.getAverageActivityByDate("2019/06/17", "2019/06/21", "numSteps").to.equal(4330));
+    expect(activity3.getAverageActivityByDate("2019/06/17", "2019/06/21", "flightsOfStaris").to.equal(7));
+    expect(activity3.getAverageActivityByDate("2019/06/17", "2019/06/21", "minutesActive").to.equal(164));
+  })
 })

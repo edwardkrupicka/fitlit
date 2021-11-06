dbad323bcd5e2b46b5656d9cef1df320aee5aae6
jose Tovar <jose.tovar102397@gmail.com>
2021-11-06 16:47:26 -0600

    Add query selectors to domUpdates file

---
+++ src/domUpdates.js
@@ -0,0 +1,24 @@
+import { makeSingleChart, makeDoubleChart } from './charts.js';
+// ~~~~~used to render user and user data~~~~~
+const userGreeting = document.querySelector('#userGreeting');
+const userFullName = document.querySelector('#userFullName');
+const userEmail = document.querySelector('#userEmail');
+const userAddress = document.querySelector('#userAddress');
+const userStride = document.querySelector('#userStride');
+const userFriends = document.querySelector('#userFriends');
+const userStepGoal = document.querySelector('#userStepGoal');
+const averageStepGoal = document.querySelector('#averageStepGoal');
+// ~~~~~~render Sleep Method~~~~~~~~
+const lastNightSleep = document.querySelector('#lastNightSleep');
+const averageSleep = document.querySelector('#averageSleep');
+// ~~~~~charts~~~~~~~~~~
+const sleepChart = document.querySelector('#sleepChart');
+const hydrationChart = document.querySelector('#hydrationChart');
+// ~~~~~~~used to render hydration~~~~~~~~
+const dailyHydration = document.querySelector('#dailyHydration');
+
+let domUpdates = {
+  
+}
+
+export default  domUpdates;
